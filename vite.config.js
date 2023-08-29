5fa6dd9607e88d3c1ba3e6c23faba1137d13e961
vladimirSEOadv <vladimirSEOadv@gmail.com>
2023-08-29 22:21:03 +0300

    changed deploy yml

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  entry: "./dist/index.html",
   base: "/firebase-auth/",
 });
