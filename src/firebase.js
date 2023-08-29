6de97ce3e3d6894d61dab4052fce466e98df39b6
vladimirSEOadv <vladimirSEOadv@gmail.com>
2023-08-29 23:12:49 +0300

    changed deploy.yml

--- src/firebase.js
+++ src/firebase.js
@@ -9,6 +9,19 @@ const {
   VITE_APP_ID,
 } = import.meta.env;
 
+[
+  VITE_API_KEY,
+  VITE_AUTH_DOMAIN,
+  VITE_PROJECT_ID,
+  VITE_STORAGE_BUCKET,
+  VITE_MESSAGING_SENDER_ID,
+  VITE_APP_ID,
+].forEach((value) => {
+  if (!value) {
+    throw new Error(`check env environment. Something was not found`);
+  }
+});
+
 const firebaseConfig = {
   apiKey: VITE_API_KEY,
   authDomain: VITE_AUTH_DOMAIN,
